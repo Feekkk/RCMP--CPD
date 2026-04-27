d28fca63d74e5ffb9793c2fdf34ddf63d06e9339
Feekkk <zxduke350@gmail.com>
2026-04-27 12:24:33 +0800

    add: staff path

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,10 @@ import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import Index from "./pages/Index.tsx";
 import Login from "./pages/login.tsx";
-import { Dashboard } from "./staff/dashboard.tsx";
+import { StaffDashboardPage } from "./staff/dashboard.tsx";
+import { History } from "./staff/History.tsx";
+import { Requisition } from "./staff/Requisition.tsx";
+import { Settings } from "./staff/Settings.tsx";
 import NotFound from "./pages/NotFound.tsx";
 
 const queryClient = new QueryClient();
@@ -17,11 +20,20 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
+          
+          {/* Homepage */}
           <Route path="/" element={<Index />} />
+
+          {/* Staff Routes */}
+          <Route path="/staff/dashboard" element={<StaffDashboardPage />} />
+          <Route path="/staff/requisition" element={<Requisition />} />
+          <Route path="/staff/history" element={<History />} />
+          <Route path="/staff/settings" element={<Settings />} />
+
+          {/* Pages */}
           <Route path="/login" element={<Login />} />
-          <Route path="/staff/dashboard" element={<Dashboard />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
+
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
